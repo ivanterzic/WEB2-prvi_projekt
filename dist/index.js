aa81c7c165e4dc54cf9ccc24784d19de5a8c0791
ivanterzic <ivan.terzic@fer.hr>
2023-10-17 22:46:10 +0200

    Basic Auth0 setup done

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var path = require('path');
 var app = express();
+var express_openid_connect_1 = require("express-openid-connect");
+require('dotenv').config();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(function (req, res) {
-    res.render('index');
+var PORT = process.env.PORT || 4010;
+var config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: 'http://localhost:4010',
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: 'https://dev-qoz5mzp8da7n1lqw.us.auth0.com'
+};
+app.use((0, express_openid_connect_1.auth)(config));
+app.get('/', function (req, res) {
+    //I want to render the before written code to index view
+    res.render('index', { title: (req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out') });
+});
+app.get('/profile', (0, express_openid_connect_1.requiresAuth)(), function (req, res) {
+    res.render('index', { title: (JSON.stringify(req.oidc.user)) });
 });
 // Start the server
-var PORT = process.env.PORT || 4010;
 app.listen(PORT, function () {
     console.log("App listening on port ".concat(PORT));
     console.log('Press Ctrl+C to quit.');
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,34 @@ import dotenv from 'dotenv';
 import {auth, requiresAuth} from 'express-openid-connect';
 import https from 'https'; 
 
+require('dotenv').config();
+
 app.set('views', path.join( __dirname, 'views'));
 app.set('view engine', 'ejs');  
  
-app.use((req, res) => { 
-    res.render('index');
-}); 
+const PORT = process.env.PORT || 4010;
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: 'http://localhost:4010',
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: 'https://dev-qoz5mzp8da7n1lqw.us.auth0.com'
+};
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+    //I want to render the before written code to index view
+    res.render('index', { title: (req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')});
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.render('index', { title: (JSON.stringify(req.oidc.user))});
+});
 
 // Start the server
-const PORT = process.env.PORT || 4010;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
