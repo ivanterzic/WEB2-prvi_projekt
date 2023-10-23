f9c1587faaad489b5680737a808502c7ab1a3893
ivanterzic <ivan.terzic@fer.hr>
2023-10-23 17:14:31 +0200

    minor redirect change

--- src/routes/tournament.routes.ts
+++ src/routes/tournament.routes.ts
@@ -70,8 +70,7 @@ tournamentRoute.post('/', requiresAuth(), [
         return
     }
     if (errorsArray.length > 0) {
-        res.render('tournament', { username: (req.oidc.user?.name), picture: (req.oidc.user?.picture), tournamentName : parsedTournament?.competitionName, rounds : parsedTournament?.rounds, table : matchesToTableElement(parsedTournament?.rounds, parsedTournament?.competitors) ,tournamentid : parsedTournament?.tournamentId, error:errorsArray[0].msg, url : req.protocol + '://' + req.get('host') + req.originalUrl});
-        return;
+        res.redirect(`/tournament?code=${req.body.tournamentid}`);
     }
     for (let match of parsedTournament.rounds){
         if (match.team1 == req.body.team1 && match.team2 == req.body.team2){
